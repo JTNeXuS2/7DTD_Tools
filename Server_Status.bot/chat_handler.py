a495a49be31915b4e3223e5d0e3ad5005887c9e6
⎛⎝Иллидан⎠⎞ <88918931+JTNeXuS2@users.noreply.github.com>
2025-09-06 12:02:35 +0200

    Update chat_handler.py

--- Server_Status.bot/chat_handler.py
+++ Server_Status.bot/chat_handler.py
@@ -78,16 +78,20 @@ def get_vehicle_list(tn, entity_id):
         return None
 
 async def handle_chat_line(tn, line):
-
-    match = re.search(r"Chat \(from 'Steam_(\d+)', entity id '(\d+)', to 'Global'\): '([^']+)':\s*(/[\w]+)", line)
+    match = re.search(r"Chat \(from 'Steam_(\d+)', entity id '(\d+)', to 'Global'\): '([^']+)':\s*(/.+)", line)
+    if not match:
+        #1CSMM_Patrons
+        match = re.search(r"Chat handled by mod '1CSMM_Patrons': Chat \(from 'Steam_(\d+)', entity id '(\d+)', to 'Global'\): '([^']+)':\s*(/.+)",line)
     if not match:
         return
+
     #print(f"ChatLine> {line}")
 
     steam_id = match.group(1)
     entity_id = match.group(2)
     name = match.group(3)
     command = match.group(4).strip().lower()
+    raw_command = match.group(4)
 
     data = load_data()
     player = data.get(steam_id, {})
@@ -95,7 +99,7 @@ async def handle_chat_line(tn, line):
     player["name"] = name
     data[steam_id] = player
 
-    if command == "/loc":
+    if command.startswith("/loc"):
         loc = get_player_location(tn, steam_id)
         if loc:
             player["base"] = loc
@@ -103,60 +107,60 @@ async def handle_chat_line(tn, line):
         else:
             send_to_server(tn, f"pm {entity_id} \"[ADFF2F]Failed to get your current location\"")
 
-    elif command == "/help":
+    elif command.startswith("/help"):
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]Commands: /help, /loc, В меню Esc (корзинка) подробности"')
 
-    elif command == "/cc":
+    elif command.startswith("/cc"):
         send_to_server(tn, f'getbicycle {entity_id}')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
         
-    elif command == "/bc":
+    elif command.startswith("/bc"):
         send_to_server(tn, f'getbike {entity_id}')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
         
-    elif command == "/lc":
+    elif command.startswith("/lc"):
         send_to_server(tn, f'getblimp {entity_id}')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
         
-    elif command == "/dc":
+    elif command.startswith("/dc"):
         send_to_server(tn, f'getdrone {entity_id}')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
         
-    elif command == "/gc":
+    elif command.startswith("/gc"):
         send_to_server(tn, f'getgyrocopter {entity_id}')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
         
-    elif command == "/hc":
+    elif command.startswith("/hc"):
         send_to_server(tn, f'gethelicopter {entity_id}')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
         
-    elif command == "/jc":
+    elif command.startswith("/jc"):
         send_to_server(tn, f'getjeep {entity_id}')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
         
-    elif command == "/mc":
+    elif command.startswith("/mc"):
         send_to_server(tn, f'getmotorcycle {entity_id}')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
 
-    elif command == "/deldrone":
+    elif command.startswith("/deldrone"):
         send_to_server(tn, f'resetdronedata {entity_id}')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
         
-    elif command == "/delchar":
+    elif command.startswith("/delchar"):
         send_to_server(tn, f'resetplayerdata {entity_id}')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
 
-    elif command == "/7day":
+    elif command.startswith("/7day"):
         day7 = re.sub(r':red_circle:|:full_moon:', '', sys.modules['__main__'].__dict__.get('day7', '∞'))
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: [FF0000]Blood Moon HORDE -[ADFF2F] {day7}"')
@@ -171,14 +175,14 @@ async def handle_chat_line(tn, line):
             print("Функция send_from_buffer_to_discord не найдена в главном модуле")
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
 
-    elif command == "/resetstats":
+    elif command.startswith("/resetstats"):
         send_to_server(tn, f'st-spk {entity_id} 0')
         send_to_server(tn, f'st-szk {entity_id} 0')
         send_to_server(tn, f'st-deaths {entity_id} 0')
         time.sleep(1)
         send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Command Complete"')
 
-    elif command == "/vc":
+    elif command.startswith("/vc"):
         vehicles_list = get_vehicle_list(tn, entity_id)
         if vehicles_list:
             for v in vehicles_list:
@@ -188,7 +192,7 @@ async def handle_chat_line(tn, line):
             send_to_server(tn, f'pm {entity_id} "[ADFF2F]: Vehicles not found"')
 
     '''
-    if command == "/setbase":
+    if command.startswith("/setbase"):
         loc = get_player_location(tn, steam_id)
         if loc:
             player["base"] = loc
@@ -196,7 +200,7 @@ async def handle_chat_line(tn, line):
         else:
             send_to_server(tn, f"say \"Failed to get your current location to set base.\"")
 
-    elif command == "/base":
+    elif command.startswith("/base"):
         if "base" in player:
             current_loc = get_player_location(tn, steam_id)
             if current_loc:
@@ -209,7 +213,7 @@ async def handle_chat_line(tn, line):
         else:
             send_to_server(tn, "say \"No base set. Use /setbase first.\"")
 
-    elif command == "/return":
+    elif command.startswith("/return"):
         if "return" in player:
             ret_loc = player.pop("return")  # Remove it after use
             send_to_server(tn, f"say \"Returning to X: {ret_loc['x']:.1f}, Y: {ret_loc['y']:.1f}, Z: {ret_loc['z']:.1f}\"")
